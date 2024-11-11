c34ed3c86f44f746a6d9bc474484147ddfdb1aa6
Til Blechschmidt <me@tibl.dev>
2024-11-11 20:50:58 +0100

    Add support for document splitting

--- src/paperless.rs
+++ src/paperless.rs
@@ -8,7 +8,8 @@ use std::{
     borrow::Cow,
     time::{Duration, Instant},
 };
-use tokio::time::sleep;
+use tokio::{task::JoinSet, time::sleep};
+use uuid::Uuid;
 
 type CowStr = Cow<'static, str>;
 
@@ -44,7 +45,10 @@ impl PaperlessClient {
         S: Into<CowStr>,
     {
         Ok(Self {
-            http_client: Client::new(),
+            http_client: reqwest::ClientBuilder::new()
+                .danger_accept_invalid_certs(true)
+                .build()
+                .unwrap(), // Client::new(),
             endpoint: endpoint.into_url()?,
             token: token.into(),
             custom_fields: CustomFieldsPatch { custom_fields },
@@ -63,9 +67,10 @@ impl PaperlessClient {
         let url = self.url(&["api", "documents", "post_document", ""]);
 
         let title = title.into();
+        let file_name_prefix = format!("EpicPrinter-{title}");
 
         let file = Part::stream(body)
-            .file_name(format!("EpicPrinter-{title}.pdf"))
+            .file_name(format!("{file_name_prefix}.pdf"))
             .mime_str("application/pdf")?;
 
         let form = multipart::Form::new()
@@ -80,7 +85,7 @@ impl PaperlessClient {
             .send()
             .await?
             .error_for_status()?
-            .json::<String>()
+            .json::<Uuid>()
             .await?;
 
         debug!("{title}\tUpload complete (task = {upload_id})");
@@ -95,8 +100,11 @@ impl PaperlessClient {
         // This runs the risk of not finishing the processing without the
         // user noticing but the impact is rather low so whatever :D
         tokio::spawn(async move {
-            match client.processing_task(&upload_id).await {
-                Ok(id) => debug!("{title}\tProcessing complete (doc = {id})"),
+            match client
+                .wait_for_processing(&upload_id, &file_name_prefix)
+                .await
+            {
+                Ok(_) => debug!("{title}\tProcessing complete"),
                 Err(err) => error!("{title}\t{err}"),
             }
         });
@@ -104,24 +112,16 @@ impl PaperlessClient {
         Ok(())
     }
 
-    async fn processing_task(
+    async fn set_document_attributes(
         &self,
-        upload_id: &str,
-    ) -> Result<DocumentID, Box<dyn std::error::Error + Send + Sync>> {
-        let document_id = self.wait_for_processing(&upload_id).await?;
-
-        self.set_custom_fields(&document_id).await?;
-
-        Ok(document_id)
-    }
-
-    async fn set_custom_fields(&self, document_id: &DocumentID) -> reqwest::Result<()> {
+        document_id: DocumentID,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         if self.custom_fields.custom_fields.is_empty() {
             return Ok(());
         }
 
         self.http_client
-            .patch(self.url(&["api", "documents", document_id, ""]))
+            .patch(self.url(&["api", "documents", &document_id, ""]))
             .header("Authorization", format!("Token {}", self.token))
             .json(&self.custom_fields)
             .send()
@@ -133,29 +133,105 @@ impl PaperlessClient {
 
     async fn wait_for_processing(
         &self,
-        upload_id: &str,
-    ) -> Result<DocumentID, Box<dyn std::error::Error + Send + Sync>> {
+        upload_id: &TaskID,
+        file_name_prefix: &str,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        debug!("{upload_id:?} Waiting for processing");
+        let result = self
+            .wait_for_task(upload_id, Duration::from_secs(5), Duration::from_secs(300))
+            .await?;
+
+        match result {
+            TaskResult::Success(document_id) => self.set_document_attributes(document_id).await,
+            TaskResult::Failure(message) => Err(message.into()),
+
+            TaskResult::Split => {
+                let mut tasks = JoinSet::new();
+
+                debug!("{upload_id:?} Handling split processing");
+                for task in self.find_related_tasks(file_name_prefix).await? {
+                    if task.uuid == *upload_id {
+                        continue;
+                    }
+
+                    debug!("{upload_id:?} Spawning split task {}", task.uuid);
+                    let client = self.clone();
+                    tasks.spawn(async move { client.wait_for_split_processing(&task.uuid).await });
+                }
+
+                tasks
+                    .join_all()
+                    .await
+                    .into_iter()
+                    .reduce(|acc, result| acc.or(result))
+                    .expect("No child tasks for split upload")
+            }
+        }
+    }
+
+    async fn wait_for_split_processing(
+        &self,
+        split_id: &TaskID,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let result = self
+            .wait_for_task(split_id, Duration::from_secs(5), Duration::from_secs(300))
+            .await?;
+
+        match result {
+            TaskResult::Success(document_id) => self.set_document_attributes(document_id).await,
+            TaskResult::Failure(message) => Err(message.into()),
+            TaskResult::Split => unreachable!("Split of a split should not be possible"),
+        }
+    }
+
+    async fn wait_for_task(
+        &self,
+        id: &TaskID,
+        interval: Duration,
+        timeout: Duration,
+    ) -> Result<TaskResult, Box<dyn std::error::Error + Send + Sync>> {
         let start = Instant::now();
 
-        while start.elapsed() < Duration::from_secs(30) {
-            if let Some(id) = self.document_id(upload_id).await? {
-                return Ok(id);
+        debug!("{id:?} Waiting for task");
+
+        while start.elapsed() < timeout {
+            let task = self.fetch_task(id).await?;
+
+            if let Some(result) = task.result() {
+                debug!("{id:?} Task completed");
+                return Ok(result);
             }
 
-            sleep(Duration::from_secs(1)).await;
+            // TODO Use exponential backoff
+            sleep(interval).await;
         }
 
         Err("Timeout while waiting for processing.".into())
     }
 
-    async fn document_id(
+    async fn find_related_tasks(
         &self,
-        upload_id: &str,
-    ) -> Result<Option<DocumentID>, Box<dyn std::error::Error + Send + Sync>> {
-        let mut url = self.url(&["api", "tasks", ""]);
-        url.set_query(Some(&format!("task_id={upload_id}")));
+        file_name_prefix: &str,
+    ) -> Result<impl Iterator<Item = Task> + '_, Box<dyn std::error::Error + Send + Sync>> {
+        let tasks = self.fetch_tasks().await?;
+        let file_name_prefix = file_name_prefix.to_string();
 
-        let mut tasks = self
+        Ok(tasks.into_iter().filter(move |task| {
+            task.file_name
+                .as_ref()
+                .map(|n| n.starts_with(&file_name_prefix))
+                .unwrap_or_default()
+        }))
+    }
+
+    async fn fetch_task(
+        &self,
+        id: &TaskID,
+    ) -> Result<Task, Box<dyn std::error::Error + Send + Sync>> {
+        let mut url = self.url(&["api", "tasks", ""]);
+        url.set_query(Some(&format!("task_id={id}")));
+
+        Ok(self
             .http_client
             .get(url)
             .header("Authorization", format!("Token {}", self.token))
@@ -163,24 +239,25 @@ impl PaperlessClient {
             .await?
             .error_for_status()?
             .json::<Vec<Task>>()
-            .await?;
+            .await?
+            .pop()
+            .ok_or(Box::<dyn std::error::Error + Send + Sync>::from(
+                "Received empty response to task query",
+            ))?)
+    }
 
-        if let Some(task) = tasks.pop() {
-            match task.status {
-                Some(TaskStatus::Started) | Some(TaskStatus::Pending) | None => Ok(None),
-                Some(TaskStatus::Success) => Ok(task.related_document),
-                Some(TaskStatus::Failure) => Err(format!(
-                    "Processing failed {}",
-                    task.result.as_ref().cloned().unwrap_or_default()
-                )
-                .into()),
-                Some(TaskStatus::Other(status)) => {
-                    Err(format!("Unknown processing status: {status}").into())
-                }
-            }
-        } else {
-            Err("Processing failed, task not found".into())
-        }
+    async fn fetch_tasks(&self) -> Result<Vec<Task>, Box<dyn std::error::Error + Send + Sync>> {
+        let url = self.url(&["api", "tasks", ""]);
+
+        Ok(self
+            .http_client
+            .get(url)
+            .header("Authorization", format!("Token {}", self.token))
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<Vec<Task>>()
+            .await?)
     }
 
     fn url(&self, segments: &[&str]) -> Url {
@@ -196,14 +273,21 @@ impl PaperlessClient {
 }
 
 type DocumentID = String;
+type TaskID = Uuid;
 
 #[derive(Deserialize)]
 struct Task {
+    #[serde(rename = "task_id")]
+    uuid: Uuid,
+
+    #[serde(default, rename = "task_file_name")]
+    file_name: Option<String>,
+
     #[serde(default)]
     status: Option<TaskStatus>,
 
-    #[serde(default)]
-    result: Option<String>,
+    #[serde(default, rename = "result")]
+    message: Option<String>,
 
     #[serde(default)]
     related_document: Option<DocumentID>,
@@ -218,4 +302,46 @@ enum TaskStatus {
     Success,
     #[serde(untagged)]
     Other(String),
+}
+
+#[derive(PartialEq, Eq)]
+enum TaskResult {
+    /// Processing failed with the given reason
+    Failure(String),
+
+    /// Input file has been split into multiple child tasks
+    Split,
+
+    /// Finished and created the given document
+    Success(DocumentID),
+}
+
+impl Task {
+    fn result(&self) -> Option<TaskResult> {
+        let message = self.message.as_ref().cloned().unwrap_or_default();
+
+        let result = match self.status.as_ref()? {
+            // Running or about to be
+            TaskStatus::Started | TaskStatus::Pending => return None,
+
+            // Input has been split into new tasks
+            TaskStatus::Success if message.contains("splitting complete") => TaskResult::Split,
+
+            // Finished successfully with a document ID
+            TaskStatus::Success if self.related_document.is_some() => {
+                TaskResult::Success(self.related_document.clone().unwrap())
+            }
+
+            // Upstream failure
+            TaskStatus::Failure => TaskResult::Failure(message),
+
+            // Missing document ID on finished task
+            TaskStatus::Success => panic!("Missing document ID on task"),
+
+            // Unknown task state which we should handle
+            TaskStatus::Other(status) => panic!("Unknown processing status: {status}"),
+        };
+
+        Some(result)
+    }
 }
